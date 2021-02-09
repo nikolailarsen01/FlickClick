4cb8f40f32a276f1ae499381eedd6805a543b93e
nikolaillarsen <niko9471@edu.sde.dk>
2021-02-09 14:37:21 +0100

    Changed controller

--- FlickClick/Controllers/CmsController.cs
+++ FlickClick/Controllers/CmsController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlickClick.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +12,8 @@ namespace FlickClick.Controllers
 {
     public class CmsController : Controller
     {
+        private string connectionString = "server=localhost;userid=root;database=steensoft_dk_flickclick;";
+
         // GET: CmsController
         public ActionResult Index()
         {
@@ -18,7 +23,34 @@ namespace FlickClick.Controllers
         // GET: CmsController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            List<MovieModel> recentTrailers = new List<MovieModel>();
+
+            string query = @"SELECT * FROM movies ORDER BY releaseDate";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataAdapter dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = cmd;
+            DataTable dtable = new DataTable();
+            dtb.Fill(dtable);
+            for (int i = 0; i < dtable.Rows.Count; i++)
+            {
+                MovieModel mm = new MovieModel();
+                mm.movieID = (int)dtable.Rows[i]["movieId"];
+                mm.title = dtable.Rows[i]["title"].ToString();
+                mm.releaseDate = (DateTime)dtable.Rows[i]["releaseDate"];
+                mm.description = dtable.Rows[i]["description"].ToString();
+                mm.directorID = (int)dtable.Rows[i]["directorID"];
+                mm.duration = dtable.Rows[i]["duration"].ToString();
+                mm.postDate = (DateTime)dtable.Rows[i]["postDate"];
+                mm.ageRating = (int)dtable.Rows[i]["ageRating"];
+                mm.comingSoon = (int)dtable.Rows[i]["ageRating"];
+                mm.picturePath = dtable.Rows[i]["picturePath"].ToString();
+                recentTrailers.Add(mm);
+            }
+
+            return View(recentTrailers);
         }
 
         // GET: CmsController/Create
@@ -26,6 +58,8 @@ namespace FlickClick.Controllers
         {
             return View();
         }
+
+
 
         // POST: CmsController/Create
         [HttpPost]
--- FlickClick/Controllers/CmsMoviesController.cs
+++ FlickClick/Controllers/CmsMoviesController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlickClick.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +13,45 @@ namespace FlickClick.Controllers
 {
     public class CmsMoviesController : Controller
     {
+
+        private readonly ILogger<HomeController> _logger;
+        private string connectionString = "server=localhost;userid=root;database=steensoft_dk_flickclick;";
+
+        public CmsMoviesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
         // GET: CmsMoviesController
         public ActionResult Index()
         {
-            return View();
+            List<MovieModel> recentTrailers = new List<MovieModel>();
+
+            string query = @"SELECT * FROM movies ORDER BY releaseDate";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataAdapter dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = cmd;
+            DataTable dtable = new DataTable();
+            dtb.Fill(dtable);
+            for (int i = 0; i < dtable.Rows.Count; i++)
+            {
+                MovieModel mm = new MovieModel();
+                mm.movieID = (int)dtable.Rows[i]["movieId"];
+                mm.title = dtable.Rows[i]["title"].ToString();
+                mm.releaseDate = (DateTime)dtable.Rows[i]["releaseDate"];
+                mm.description = dtable.Rows[i]["description"].ToString();
+                mm.directorID = (int)dtable.Rows[i]["directorID"];
+                mm.duration = dtable.Rows[i]["duration"].ToString();
+                mm.postDate = (DateTime)dtable.Rows[i]["postDate"];
+                mm.ageRating = (int)dtable.Rows[i]["ageRating"];
+                mm.comingSoon = (int)dtable.Rows[i]["ageRating"];
+                mm.picturePath = dtable.Rows[i]["picturePath"].ToString();
+                recentTrailers.Add(mm);
+            }
+
+            return View(recentTrailers);
         }
 
         // GET: CmsMoviesController/Details/5
