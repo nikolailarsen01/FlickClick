e57a4ae0212e94552523bfcb752282b73c566950
nikolaillarsen <niko9471@edu.sde.dk>
2021-02-10 13:06:13 +0100

    Added DBConnector

---
+++ FlickClick.BL/DBConnector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace FlickClick.BL
+{
+    public class DBConnector
+    {
+        private string connectionString;
+        private MySqlConnection connection;
+        public DBConnector()
+        {
+            connectionString = "server=localhost;userid=root;database=steensoft_dk_flickclick;";
+        }
+        ~DBConnector() { }
+
+        public bool makeConnection()
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection.Ping();
+        }
+        public DataTable sqlSelectQuery(string query)
+        {
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataAdapter dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = command;
+            DataTable dtable = new DataTable();
+            dtb.Fill(dtable);
+
+            return dtable;
+        }
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
