4178df23650a8d092a41144b43a497af5d68b214
Christian <Christian@DESKTOP-CIKSI95>
2021-02-17 10:27:34 +0100

    Register

--- FlickClick/Models/UserCreateModel.cs
+++ FlickClick/Models/UserCreateModel.cs
@@ -9,10 +9,13 @@ namespace FlickClick.Models
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
+        public string email { get; set; }
+        public string confirmEmail { get; set; }
         public int postalCode { get; set; }
         public string streetName { get; set; }
         public int houseNumber { get; set; }
         public string password { get; set; }
+        public string confirmPassword { get; set; }
         public int phoneNumber { get; set; }
     }
 }
