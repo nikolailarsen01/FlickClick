55a66a33e74476fe9015e23ab58106f01c04b171
Christian <Christian@DESKTOP-CIKSI95>
2021-02-18 14:04:41 +0100

    Register backend

--- FlickClick/Controllers/LoginController.cs
+++ FlickClick/Controllers/LoginController.cs
@@ -69,6 +69,80 @@ namespace FlickClick.Controllers
         [HttpPost]
         public ActionResult ValidateRegister()
         {
+            string firstName = HttpContext.Request.Form["firstName"];
+            string lastName = HttpContext.Request.Form["lastName"];
+            string email = HttpContext.Request.Form["email"];
+            int postalCode = Int32.Parse(HttpContext.Request.Form["postalCode"]);
+            string streetName = HttpContext.Request.Form["streetName"];
+            string houseNumber = HttpContext.Request.Form["houseNumber"];
+            string password = HttpContext.Request.Form["password"];
+            int phoneNumber = Int32.Parse(HttpContext.Request.Form["phoneNumber"]);
+
+            int postalCodeID = 0;
+            int streetNameID = 0;
+            int adressID = 0;
+            //postalCode
+            string query = "SELECT * FROM citycodes WHERE PostalCode='"+postalCode+"'";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataAdapter dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = cmd;
+            DataTable dtable = new DataTable();
+            dtb.Fill(dtable);
+            if (dtable.Rows.Count > 0)
+            {
+                postalCodeID = (int)dtable.Rows[0]["cityID"];
+            }
+            else
+            {
+                query = "INSERT INTO `citycodes`(`PostalCode`) VALUES ('"+postalCode+"')";
+                cmd = new MySqlCommand(query, connection);
+                postalCodeID = cmd.ExecuteNonQuery();
+            }
+
+            //streetName
+            query = "SELECT * FROM streetnames WHERE streetName='" + streetName + "'";
+            connection = new MySqlConnection(connectionString);
+            cmd = new MySqlCommand(query, connection);
+            connection.Open();
+            dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = cmd;
+            dtable = new DataTable();
+            dtb.Fill(dtable);
+            if (dtable.Rows.Count > 0)
+            {
+                streetNameID = (int)dtable.Rows[0]["streetID"];
+            }
+            else
+            {
+                query = "INSERT INTO `streetnames`(`streetName`) VALUES ('" + streetName + "')";
+                cmd = new MySqlCommand(query, connection);
+                streetNameID = cmd.ExecuteNonQuery();
+            }
+
+            //Adress
+            query = "SELECT * FROM addressjunction WHERE cityID='" + postalCodeID + "' AND streetID='" + streetNameID + "' AND houseNumber='" + houseNumber + "'";
+            connection = new MySqlConnection(connectionString);
+            cmd = new MySqlCommand(query, connection);
+            connection.Open();
+            dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = cmd;
+            dtable = new DataTable();
+            dtb.Fill(dtable);
+            if (dtable.Rows.Count > 0)
+            {
+                adressID = (int)dtable.Rows[0]["ID"];
+            }
+            else
+            {
+                query = "INSERT INTO `addressjunction`(`cityID`, `streetID`, `houseNumber`) VALUES ('"+postalCodeID+ "','" + streetNameID + "','" + houseNumber + "')";
+                cmd = new MySqlCommand(query, connection);
+                streetNameID = cmd.ExecuteNonQuery();
+            }
+
+
+
 
             return View();
         }
--- FlickClick/wwwroot/js/site.js
+++ FlickClick/wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function HomePage() {
+    window.location.href = "/Home";
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateForm(form) {
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.replace(/\s/g, '') == "") {
+            alert("Please fill all empty fields");
+            return false;
+        }
+    }
+
+    if (document.forms["registerForm"]["email"].value != document.forms["registerForm"]["confirmEmail"].value) {
+        alert("Emails do not match");
+        return false;
+    }
+
+    if (document.forms["registerForm"]["phoneNumber"].value.length != 8) {
+        alert("Invalid phone number");
+        return false;
+    }
+
+    if (document.forms["registerForm"]["password"].value != document.forms["registerForm"]["passwordEmail"].value) {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    if (!validateEmail(document.forms["registerForm"]["email"].value)) {
+        alert("Please insert a valid email");
+        return false;
+    }
+
+    return true;
+}
